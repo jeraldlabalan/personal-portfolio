0f88a1e29edb4ebf8e2664b8818706424a5ce2ad
Jerald Labalan <jeraldbalan26@gmail.com>
2024-11-10 00:19:37 +0800

    update main.jsx

--- jeee4/src/App.jsx
+++ jeee4/src/App.jsx
@@ -1,31 +1,32 @@
 import './App.css';
 import Home from './sections/Home/Home';
-import Skills from '/src/sections/Skills/Skills';
-import Contact from '/src/sections/Contact/Contact';
-import Footer from '/src/sections/Footer/Footer';
+import Skills from './sections/Skills/Skills';
+import Contact from './sections/Contact/Contact';
+import Footer from './sections/Footer/Footer';
 import Header from './sections/Header/Header';
 import About from './sections/About/About';
 import Qualification from './sections/Qualification/Qualification';
 import ScrollDown from './sections/Home/ScrollDown';
 import ScrollUp from './sections/ScrollUp/ScrollUp';
 
-
 function App() {
   return (
-  <>
-    <Header />
-    
-    <main className='main'>
-    <Home />
-    <About />
-    <ScrollDown />
-    <Skills />
-    <Qualification />
-    <Contact />
-    </main>
-    <Footer />
-    <ScrollUp />
-  </>);
+    <>
+      <Header />
+
+      <main className='main'>
+        <Home />
+        <About />
+        <ScrollDown />
+        <Skills />
+        <Qualification />
+        <Contact />
+      </main>
+
+      <Footer />
+      <ScrollUp />
+    </>
+  );
 }
 
-export default App
+export default App;
